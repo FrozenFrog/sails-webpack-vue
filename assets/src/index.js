1e3c51e5067be8b35f0370a0808d677b9f0874c3
nhakhachdhqg <badauyeu4180@gmail.com>
2018-08-09 05:43:30 +0700

    Complete redirect when not login

--- assets/src/index.js
+++ assets/src/index.js
@@ -1,33 +1,41 @@
-import Vue from 'vue'
-import VueI18n from 'vue-i18n'
-import VTooltip from 'v-tooltip'
-import VueRouter from 'vue-router'
-import axios from 'axios'
-import VueAxios from 'vue-axios'
-import App from './templates/index.vue'
-import 'animate.css/animate.min.css'
-import 'materialize-css/dist/css/materialize.min.css'
-import 'materialize-css/dist/js/materialize.min.js'
+import Vue from "vue";
+import VueI18n from "vue-i18n";
+import VTooltip from "v-tooltip";
+import VueRouter from "vue-router";
+import axios from "axios";
+import VueAxios from "vue-axios";
+import App from "./templates/index.vue";
+import "animate.css/animate.min.css";
+import "materialize-css/dist/css/materialize.min.css";
+import "materialize-css/dist/js/materialize.min.js";
 
-import {routes} from './routes'
-Vue.use(VueI18n)
-Vue.use(VTooltip)
-Vue.use(VueRouter)
-axios.defaults.baseURL = "http://localhost:1337/"
-Vue.use(VueAxios, axios)
+import { routes } from "./routes";
+Vue.use(VueI18n);
+Vue.use(VTooltip);
+Vue.use(VueRouter);
+axios.defaults.baseURL = "http://localhost:1337/";
+Vue.use(VueAxios, axios);
 
 const router = new VueRouter({
-    routes
+  routes
+})
+
+router.beforeEach((to, from, next) => {
+    if (to.matched.some(record => record.meta.requiresAuth)) {
+      next('Login')
+    }
+    if (!to.matched.length) next('Index')
+    else next()
 })
 
 const i18n = new VueI18n({
-    locale: 'vn', // set locale
-    fallbackLocale: 'vn'
-})
+  locale: "vn", // set locale
+  fallbackLocale: "vn"
+});
 
 new Vue({
-    el: '#root',
-    router,
-    i18n,
-    render: h => h(App)
-})
+  el: "#root",
+  router,
+  i18n,
+  render: h => h(App)
+});
--- assets/src/routes.js
+++ assets/src/routes.js
@@ -1,13 +1,25 @@
-import Login from './templates/User/Login.vue'
-import Index from './templates/index.vue'
-import DisplayItems from './templates/Items/DisplayItems.vue'
+import Login from "./templates/User/Login.vue";
+import Index from "./templates/index.vue";
+import DisplayItems from "./templates/Items/DisplayItems.vue";
 export const routes = [
-    {path: '/login', component: Login, name: 'Login'},
-    {path: '/item', component: DisplayItems, name: 'DisplayItems', 
-        meta: { 
-            requiresAuth: true
-        }
-    },
-    {path: '/', component: Index, name: 'Index'}
-]
-
+  {
+    path: "/login",
+    component: Login,
+    name: "Login"
+  },
+  {
+    path: "/",
+    component: Index,
+    name: "Index",
+    meta: {
+      requiresAuth: true,
+      isAdmin: 
+    }
+  },
+  {
+      /* Redirect to login page if cannot find valid URL
+      I'm thinking that Login page will check if isLogin next to Index */
+    path: "*", 
+    redirect: 'Login'
+  }
+];
